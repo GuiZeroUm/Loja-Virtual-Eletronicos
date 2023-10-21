b0edca7b0cee49af42f42d4d9c48aedb41b42035
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-21 00:11:36 -0500

    feat: adicionando componentes do botao, card e header

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,21 @@
+import { MenuIcon, ShoppingCart } from "lucide-react";
+import { Button } from "./button";
+import {Card} from "./card";
+
+const Header = () => {
+    return (
+    <Card className="flex justify-between p-[1.875rem]">
+        
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+        <h1 className="font-semibold text-lg">
+            <span className="text-primary">FSW</span> Store</h1>
+        <Button size="icon" variant="outline">
+            <ShoppingCart />
+        </Button>
+
+    </Card>
+);};
+
+export default Header;
