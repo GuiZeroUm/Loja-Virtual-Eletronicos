44e90971e18ed1d7fef5d63695cfa23b6041978f
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-21 13:05:09 -0500

    add sheet tsx

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,24 @@
-import { MenuIcon, ShoppingCart } from "lucide-react";
+import { MenuIcon, Sheet, ShoppingCart } from "lucide-react";
 import { Button } from "./button";
 import {Card} from "./card";
+import { SheetContent, SheetTrigger } from "./sheet";
 
 const Header = () => {
     return (
     <Card className="flex justify-between p-[1.875rem]">
-        
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button size="icon" variant="outline">
+                    <MenuIcon/>
+                </Button>
+            </SheetTrigger>
+
+            <SheetContent side="left">
+                <h1>Menu</h1>
+            </SheetContent>
+        </Sheet>
+
         <h1 className="font-semibold text-lg">
             <span className="text-primary">FSW</span> Store</h1>
         <Button size="icon" variant="outline">
