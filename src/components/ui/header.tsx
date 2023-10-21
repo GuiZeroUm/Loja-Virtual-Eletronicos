e6a1d87759a50b2d21a5219248592cb28a6ec424
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-21 13:28:22 -0500

    fix: all the header and sheet bugs

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,31 +1,37 @@
-import { MenuIcon, Sheet, ShoppingCart } from "lucide-react";
+import {
+  Car,
+  CarIcon,
+  MenuIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./button";
-import {Card} from "./card";
-import { SheetContent, SheetTrigger } from "./sheet";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetTrigger } from "./sheet";
 
 const Header = () => {
-    return (
-    <Card className="flex justify-between p-[1.875rem]">
+return (
+  <Card className="flex items-center justify-between p-[1.875rem]">
+    <Sheet>
+      <SheetTrigger asChild>
+      <Button size="icon" variant="outline">
+      <MenuIcon />
+    </Button>
+      </SheetTrigger>
 
-        <Sheet>
-            <SheetTrigger asChild>
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
-                </Button>
-            </SheetTrigger>
+      <SheetContent side="left">
+        <h1>Menu</h1>
+      </SheetContent>
+    </Sheet>
 
-            <SheetContent side="left">
-                <h1>Menu</h1>
-            </SheetContent>
-        </Sheet>
+    <h1 className="text-lg font-semibold">
+      <span className="text-primary">FSW</span> Store
+    </h1>
 
-        <h1 className="font-semibold text-lg">
-            <span className="text-primary">FSW</span> Store</h1>
-        <Button size="icon" variant="outline">
-            <ShoppingCart />
-        </Button>
-
-    </Card>
-);};
+    <Button size="icon" variant="outline">
+      <ShoppingCartIcon />
+    </Button>
+  </Card>
+)
+};
 
 export default Header;
