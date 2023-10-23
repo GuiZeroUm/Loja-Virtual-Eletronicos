184a32624dce368661c60054084d4e520b41ac1f
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-22 19:25:00 -0500

    feat: add Categories and CategoriesItem component

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,16 @@
+import { prismaClient } from "@/lib/prisma";
+import CategoryItem from "./category-item";
+import { Key } from "lucide-react";
+
+const Categories = async () => {
+    const categories = await prismaClient.category.findMany({})
+    return (
+        <div className="grid grid-cols-2 gap-y-2 gap-x-4">
+            {categories.map((category) =>(
+                <CategoryItem key={category.id} category={category}/>
+            ))}
+        </div>
+    );
+}
+ 
+export default Categories;
---
+++ src/app/(home)/components/category-item.tsx
@@ -0,0 +1,26 @@
+import { Badge } from "@/components/ui/badge";
+import { Category } from "@prisma/client";
+import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
+
+interface CategoryItemProps {
+    category: Category;
+}
+
+const CategoryItem = ({category}: CategoryItemProps) => {
+    const CategoryIcon = {
+        keyboards: <KeyboardIcon size={16}/>,
+        monitors: <MonitorIcon size={16}/>,
+        headphones: <HeadphonesIcon size={16}/>,
+        mousepads: <SquareIcon size={16}/>,
+        speakers: <SpeakerIcon size={16}/>,
+        mouses: <MouseIcon size={16}/>
+    }
+    return (  
+        <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
+            {CategoryIcon[category.slug as keyof typeof CategoryIcon]}
+            <span className="font-bold text-xs">{category.name}</span>
+        </Badge>
+    );
+}
+ 
+export default CategoryItem;
