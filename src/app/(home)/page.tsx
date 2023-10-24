e5edaabcd348e1d9ada34e97023812122bd73a83
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-24 16:46:52 -0500

    feat: add mouse section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,6 +22,14 @@ export default async function Home() {
     },
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div className='flex flex-col gap-8'>
     <PromoBanner
@@ -55,6 +63,11 @@ export default async function Home() {
       src="/banner-home-03.png"
       alt="Até 55% de desconto em mouses!"
     />
+    </div>
+    
+    <div>
+      <SectionTitle>Mouses</SectionTitle>
+      <ProductList products={mouses}/>
     </div>
   </div>
   );
