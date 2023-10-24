7198a3964cb86b4f52704cbf356400119d976048
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-24 16:57:37 -0500

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+            © 2023 Copyright <a href="https://github.com/GuiZeroUm" className="font-semibold">GuiZeroUm</a>
+        </div>
+    );
+}
+ 
+export default Footer;
