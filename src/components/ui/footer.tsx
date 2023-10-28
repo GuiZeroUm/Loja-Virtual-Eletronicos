e9642e4fe1f0c7e2ffc47c54dbcb58043d596b70
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-28 18:09:04 -0500

    fix: mod the footer link

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
       <div className="mx-auto lg:container">
-        © 2023 Copyright <a className="font-semibold" href="github.com/GuiZeroUm">FSW Store</a>
+        © 2023 Copyright <a className="font-semibold" href='https://github.com/GuiZeroUm'>DevGuiZeroUm</a>
       </div>
     </div>
   );
